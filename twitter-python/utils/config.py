d60794ba07ab3c81d30e3bae98c042337cbae5de
SHIN0003 <andrewtai@umass.edu>
2024-12-27 00:05:30 -0500

    API environment set up

--- twitter-python/utils/config.py
+++ twitter-python/utils/config.py
@@ -7,8 +7,8 @@ KAFKA_BROKER_URL = os.environ.get('KAFKA_BROKER_URL', 'kafka:9092')
 KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', 'reddit_comments')
 
 # Reddit API Configuration
-REDDIT_CLIENT_ID = os.environ.get('REDDIT_CLIENT_ID')
-REDDIT_CLIENT_SECRET = os.environ.get('REDDIT_CLIENT_SECRET')
+REDDIT_CLIENT_ID = os.environ.get('CLIENT_ID')
+REDDIT_CLIENT_SECRET = os.environ.get('SECRET')
 REDDIT_USER_AGENT = os.environ.get('REDDIT_USER_AGENT', 'reddit-kafka-producer')
 
 # Subreddit to Monitor
