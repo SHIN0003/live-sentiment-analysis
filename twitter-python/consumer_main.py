949b5891c197e1d48008d3c7b1f3cc8f51ca3685
SHIN0003 <andrewtai@umass.edu>
2024-12-31 21:16:13 -0500

    consumer now sends processed data back to kafka

--- twitter-python/consumer_main.py
+++ twitter-python/consumer_main.py
@@ -3,7 +3,9 @@ import nltk
 import db
 from consumer import return_consume_obj
 from sentiment import analyze
-
+from producer import return_producer_obj
+import time
+import json
 
 def main():
     print("Waiting for messages...")
@@ -17,10 +19,20 @@ def main():
     db.close_connection(conn, cursor)
 
     consumer = return_consume_obj()
+    producer = return_producer_obj()
+    
+    
     print(consumer)
     for message in consumer:
-        print(message.key.decode("utf-8"))
-        print(analyze(message.value.decode("utf-8")))
+        try:
+            resmessage = message.value.decode("utf-8")
+            reskey = message.key.decode("utf-8")
+            print(reskey, resmessage)
+            res = analyze(resmessage)
+            producer.send("processed-data",value=json.dumps(res, indent=2).encode('utf-8'), key=message.key)
+        except Exception as e:
+            print(f"Error sending message: {e}")
+            time.sleep(5)
 
 
 if __name__ == "__main__":
