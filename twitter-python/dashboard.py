9f92502f26fdfcd6314acd8e372e9040c72e4f29
SHIN0003 <andrewtai@umass.edu>
2025-01-04 12:13:21 -0500

    sanity check dashboard

--- twitter-python/dashboard.py
+++ twitter-python/dashboard.py
@@ -7,5 +7,12 @@ import db
 st.title("Reddit Sentiment Analysis")
 st.title("testing live")
 
-conn = st.connection("postgresql", type="sql")
-
+st.write("Waiting for messages...")
+conn = db.return_connection()
+st.write("Connected to the database!")
+cursor = conn.cursor()
+# Example query
+cursor.execute("SELECT version();")
+db_version = cursor.fetchone()
+st.write("PostgreSQL version:", db_version)
+db.close_connection(conn, cursor)
